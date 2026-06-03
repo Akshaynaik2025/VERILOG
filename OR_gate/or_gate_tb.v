b4915734b397f7cc80a3c9b54ffe361e244dd82e
Akshay Naik <YOUR_GITHUB_EMAIL>
2026-06-03 23:26:12 +0530

    Added OR gate project

---
+++ OR_gate/or_gate_tb.v
@@ -0,0 +1,22 @@
+`timescale 1ns/1ps
+module or_gate_tb;
+	reg a,b;
+	wire y;
+or_gate G1(
+	.a(a),
+	.b(b),
+	.y(y)
+);
+initial begin
+$dumpfile("or_gate.vcd");
+$dumpvars(0,or_gate_tb);
+
+$display("A B | Y");
+$monitor("%b %b|%b",a,b,y);
+a=0;b=0;#10;
+a=0;b=1;#10;
+a=1;b=0;#10;
+a=1;b=1;#10;
+$finish;
+end
+endmodule;
